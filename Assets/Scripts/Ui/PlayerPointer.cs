c4401f8c7010c29fde41a6dd311db22ac35e5aa9
exe4que <exe4que@gmail.com>
2024-08-21 00:37:58 -0300

    turbo ready ui animation implemented

--- Assets/Scripts/Ui/PlayerPointer.cs
+++ Assets/Scripts/Ui/PlayerPointer.cs
@@ -1,4 +1,6 @@
+using DG.Tweening;
 using RaceGame.Gameplay;
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,6 +22,27 @@ namespace RaceGame.Ui
         private Image _pointerImage;
         [SerializeField]
         private Image _backgroundImage;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private float _turboReadyScaleUpDuration = 0.5f;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private float _turboReadyScaleUpSize = 2f;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private Ease _turboReadyScaleUpEase = Ease.OutBack;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private float _turboReadyFadeDuration = 0.5f;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private Ease _turboReadyFadeEase = Ease.InOutSine;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private float _turboReadyPunchDuration = 0.2f;
+        [BoxGroup("Turbo Ready Animation")]
+        [SerializeField]
+        private float _turboReadyPunchSize = 0.1f;
         
         private Transform _carTransform;
         private bool _isInitialized = false;
@@ -27,6 +50,8 @@ namespace RaceGame.Ui
         private int _id;
         private float _maxPointerSize = 0f;
         private float _normalPointerSize = 0f;
+        private PointerState _pointerState = PointerState.Normal;
+        private Color _backgroundOriginalColor;
         
         public void Initialize(int id, Transform carTransform, Camera mainCamera)
         {
@@ -35,6 +60,7 @@ namespace RaceGame.Ui
             _carTransform = carTransform;
             _pointerImage.color = Colors[id];
             _mainCamera = mainCamera;
+            _backgroundOriginalColor = _backgroundImage.color;
             _isInitialized = true;
             
             _normalPointerSize = _pointerImage.rectTransform.sizeDelta.x;
@@ -53,26 +79,79 @@ namespace RaceGame.Ui
             }
         }
 
+        Sequence _turboReadySequence;
         private void HandleTurboAnimations()
         {
             if (TrackManager.Instance.IsTurboReady(_id))
             {
+                if(_pointerState != PointerState.TurboReady)
+                {
+                    PlayTurboReadyAnimation();
+                    _pointerState = PointerState.TurboReady;
+                }
                 _pointerImage.rectTransform.sizeDelta = new Vector2(_maxPointerSize, _maxPointerSize);
             }
             else
             {
                 if (TrackManager.Instance.IsTurboActive(_id))
                 {
-                    //TODO: animate the pointer
+                    if(_pointerState != PointerState.TurboActive)
+                    {
+                        ClearTurboReadyAnimation(true);
+                        PlayTurboActiveAnimation();
+                        _pointerState = PointerState.TurboActive;
+                    }
                     _pointerImage.rectTransform.sizeDelta = new Vector2(_normalPointerSize, _normalPointerSize);
                 }
                 else
                 {
+                    _pointerState = PointerState.Normal;
                     float normalizedCooldown = TrackManager.Instance.GetCarTurboCooldownNormalized(_id);
                     float currentSize = Utils.Remap(0f, 1f, _normalPointerSize, _maxPointerSize, 1f - normalizedCooldown);
                     _pointerImage.rectTransform.sizeDelta = new Vector2(currentSize, currentSize);
                 }
             }
+        }
+
+        private void PlayTurboActiveAnimation()
+        {
+            //todo
+        }
+
+        [Button]
+        private void PlayTurboReadyAnimation()
+        {
+            if (_turboReadySequence != null)
+            {
+                _turboReadySequence.Kill();
+            }
+            _turboReadySequence = DOTween.Sequence();
+            _turboReadySequence.Append(_backgroundImage.rectTransform
+                .DOScale(_turboReadyScaleUpSize, _turboReadyScaleUpDuration).SetEase(_turboReadyScaleUpEase));
+            _turboReadySequence.Join(_backgroundImage.DOFade(0f, _turboReadyFadeDuration)
+                .SetEase(_turboReadyFadeEase));
+            _turboReadySequence.Join(_pointerImage.rectTransform.DOPunchScale(Vector3.one * _turboReadyPunchSize, _turboReadyPunchDuration));
+            _turboReadySequence.AppendInterval(1f);
+            _turboReadySequence.AppendCallback(() => ClearTurboReadyAnimation(false));
+            _turboReadySequence.SetLoops(-1);
+        }
+
+        private void ClearTurboReadyAnimation(bool killSequence)
+        {
+            if (killSequence)
+            {
+                _turboReadySequence?.Kill();
+            }
+            _backgroundImage.rectTransform.localScale = Vector3.one;
+            _backgroundImage.color = _backgroundOriginalColor;
+            _pointerImage.rectTransform.localScale = Vector3.one;
+        }
+
+        private enum PointerState
+        {
+            Normal,
+            TurboReady,
+            TurboActive
         }
     }
 }
